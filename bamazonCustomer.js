2549ad2e59789a14f42e5d9ce4ffa45f3b5d2d3f
jdrenteria <jesusrenteria48@gmail.com>
2020-02-09 21:05:10 -0600

    added new code to connec to mysql database as well as a display fucntion for the terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,16 @@ var connection = mysql.createConnection({
     database: "bamazon_db",
     port: 3000
 })
+connection.connect();
+
+var display = function(){
+    connection.query("SELECT * FROM products", function(err,res){
+        if (err) throw err;
+        console.log("-------------------------------------------");
+        console.log("          WELCOME TO BAMAZON           ");
+        console.log("-------------------------------------------");
+        console.log("");
+        console.log(" FIND YOUR PRODUCT BELOW");
+        console.log("");
+    })
+}
